7d19d07aa1f6ecafef179d1732d7d0213cf79361
Nicholas Fraleigh <nicholasfraleigh@gmail.com>
2016-06-15 17:42:21 -0700

    display animated icons, first draft

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,67 @@
+// selectively hide elements based on weather conditions
+// display city name at top of screen
+// prettify
+// clean up code
+
+
+var tempC = 0;
+var tempF = 0;
+var tempK = 0;
+var lat = 0;
+var long = 0;
+
 $(document).ready(function(){
-    console.log("echo1");
-    fetchJsonCurrent();
-    console.log("echo4");
-    fetchJsonForecast();
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude 
+            long =  position.coords.longitude;
+            lat = Math.round(lat);
+            long = Math.round(long);
+            console.log("lat: " + lat);
+            console.log("long: " + long); 
+            fetchJsonCurrent(lat, long);
+        });
+    }
+
+
+
+    
+    
+    $("#convert").click(function(){
+        var currScale = document.getElementById('scale').innerHTML;
+        if(currScale === 'K'){
+            document.getElementById("temp").innerHTML = tempC + " <a id = 'scale'>C</a>";
+        } else if (currScale === 'C'){
+            document.getElementById("temp").innerHTML = tempF + " <a id = 'scale'>F</a>";
+        } else if (currScale === 'F'){
+            document.getElementById("temp").innerHTML = tempK + " <a id = 'scale'>K</a>";
+        } 
+    });
+
+    $("#update").click(function(){
+        fetchJsonCurrent(lat, long, function() { 
+            document.getElementById("temp").innerHTML = tempC + " <a id = 'scale'>C</a>";
+        });
+    })
+
 });
 
-
-
-
-function fetchJsonCurrent(){
+function fetchJsonCurrent(lat, long){
 	$.ajax({
-    	url: 'http://api.openweathermap.org/data/2.5/weather?id=6174041&APPID=2df7b25629785dc7245a6b0d4d47320e', // The URL to the API. You can get this in the API page of the API you intend to consume
+    	url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=2df7b25629785dc7245a6b0d4d47320e', // The URL to the API. You can get this in the API page of the API you intend to consume
     	type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
     	data: {}, // Additional parameters here
     	dataType: 'json',
     	success: function(data) { 
-    		document.getElementById("weather").innerHTML = data['weather'][0]['main'];
-            document.getElementById("description").innerHTML = data['weather'][0]['description'];
+    		document.getElementById("weather").innerHTML = data['weather'][0]['main'] + ": " + data['weather'][0]['description'];            
+            document.getElementById("temp").innerHTML = "<a id = 'scale'></a>";
+            convertTemp(data['main']['temp']);
+            // Create a new JavaScript Date object based on the timestamp
+            // multiplied by 1000 so that the argument is in milliseconds, not seconds.
             var date = new Date(data['dt']*1000);
     		document.getElementById('date').innerHTML = '~ ' + convertTime(date);
+            document.getElementById("temp").innerHTML = tempC + " <a id = 'scale'>C</a>";
     	},
     	error: function(err) { 
             alert(err); 
@@ -31,6 +74,51 @@ function fetchJsonCurrent(){
     });
 }
 
+function convertTime(date){
+    
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    if(hours > 12){
+        hours -= 12;
+    }
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saterday'];
+    var day = days[date.getDay()];
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = day + ", " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
+
+function convertTemp(curr){
+    tempC = curr - 273.15;
+    tempC = Math.round(tempC * 10) / 10;
+    tempF = curr * (9/5) - 459.67;
+    tempF = Math.round(tempF * 10) / 10;
+    tempK = Math.round(curr *10) / 10;
+
+  
+}
+
+  /*
+    var currScale = document.getElementById('scale').innerHTML;
+    if(currScale === '' || currScale === 'K'){
+        var tempC = curr - 273.15;
+        tempC = Math.round(tempC * 10) / 10;
+        document.getElementById("temp").innerHTML = tempC + " <a id = 'scale'>C</a>";
+    } else if (currScale === 'C'){
+        var 
+        document.getElementById("temp").innerHTML = tempF + " <a id = 'scale'>F</a>";
+    } else if (currScale === 'F'){
+        var tempK = (curr + 459.67) * (5/9);
+        document.getElementById("temp").innerHTML = tempK + " <a id = 'scale'>K</a>";
+    }
+    */
+
+/*
 function fetchJsonForecast(){
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/forecast?id=6174041&APPID=2df7b25629785dc7245a6b0d4d47320e', // The URL to the API. You can get this in the API page of the API you intend to consume
@@ -39,7 +127,7 @@ function fetchJsonForecast(){
         dataType: 'json',
         success: function(data) { 
             console.log(getForecast(data));
-            document.getElementById('forecast').innerHTML = '+ ' + getForecast(data);
+            document.getElementById('weather').innerHTML = '+ ' + getForecast(data);
             checkObj(data);
             //document.getElementById("forecast").innerHTML = data['weather'][0]['main'];
         },
@@ -70,28 +158,10 @@ function checkObj(obj){
         }
     }
 }
+*/
 
-function convertTime(date){
-    // Create a new JavaScript Date object based on the timestamp
-    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-    //var date = new Date(obj['dt']*1000);
-    // Hours part from the timestamp
-    var hours = date.getHours();
-    if(hours > 12){
-        hours -= 12;
-    }
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saterday'];
-    var day = days[date.getDay()];
-    // Minutes part from the timestamp
-    var minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    var seconds = "0" + date.getSeconds();
 
-    // Will display time in 10:30:23 format
-    var formattedTime = day + ", " + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return formattedTime;
-}
-
+/*
 function getForecast(obj){
     //loop to get all 5 days, put into proper format, apply to page
     var fiveDay = "Five day forecast: ";
@@ -101,4 +171,6 @@ function getForecast(obj){
     return fiveDay;
    
 }
+
+*/
 //$('#twitter').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data['quote'] + '" ~ ' + data['author']));
